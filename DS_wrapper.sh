005004cc0a266d07de350501cda895f2be18a48d
nanakiksc <polcusco@gmail.com>
2018-10-10 12:16:42 +0200

    Changed wrapper interface to iterate over a samples file.

--- DS_wrapper.sh
+++ DS_wrapper.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-I1=$1 # Input file 1 (.fastq.gz).
-I2=$2 # Input file 2 (.fastq.gz).
-SM=$3 # Sample name.
+ID=$1 # Input directory.
+SF=$2 # Samples file (contains sample names).
 
-mkdir -p FastQC
-/home/pcusco/utils/FastQC/fastqc ${I1} ${I2} --outdir FastQC
+for SM in $(cat ${SF})
+do
+    I1=${ID}/${SM}_*_R1_001.fastq.gz
+    I2=${ID}/${SM}_*_R2_001.fastq.gz
 
-./DS_part1.sh ${I1} ${I2} ${SM}
-./DS_part2.sh ${SM}_read1_sscs.fq.gz ${SM}_read2_sscs.fq.gz ${SM}.sscs
-./DS_part2.sh ${SM}_read1_dcs.fq.gz ${SM}_read2_dcs.fq.gz ${SM}.dcs
+    mkdir -p FastQC
+    /home/pcusco/utils/FastQC/fastqc ${I1} ${I2} --outdir FastQC
+
+    ./DS_part1.sh ${I1} ${I2} ${SM}
+    ./DS_part2.sh ${SM}_read1_sscs.fq.gz ${SM}_read2_sscs.fq.gz ${SM}.sscs
+    ./DS_part2.sh ${SM}_read1_dcs.fq.gz ${SM}_read2_dcs.fq.gz ${SM}.dcs
+done
 
 exit 0
